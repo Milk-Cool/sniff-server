41c2e750207fbb5ef88a4d891eb5a7b00fa6345c
Milk-Cool <coder.tomsk@gmail.com>
2025-07-09 10:52:18 +0300

    fix auth error

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import { statsRouter } from "./stats.js";
 
 export const apiRouter = express.Router();
 apiRouter.use(async (req, res, next) => {
-    if(req.headers.authorization.replace("Bearer ", "") !== process.env.ADMIN_KEY)
+    if(req.headers.authorization?.replace?.("Bearer ", "") !== process.env.ADMIN_KEY)
         return res.status(401).send("Unauthorized!");
     return await next();
 });
