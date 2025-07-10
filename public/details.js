9bc9ca44481d1a57a812d5626ff5e462f83cd7f3
Milk-Cool <coder.tomsk@gmail.com>
2025-07-10 10:53:00 +0300

    add datetime selector for details

--- public/details.js
+++ public/details.js
@@ -2,7 +2,7 @@ let offset = 0;
 const timeline = document.querySelector("#timeline");
 const pullTimeline = async () => {
     const query = new URLSearchParams(location.search);
-    const f = await fetch(`/api/details?mac=${encodeURIComponent(query.get("mac"))}&offset=${offset}`, {
+    const f = await fetch(`/api/details?mac=${encodeURIComponent(query.get("mac"))}&offset=${offset}${query.has("from") ? "&from=" + query.get("from") : ""}${query.has("to") ? "&to=" + query.get("to") : ""}`, {
         headers: { authorization: "Bearer " + localStorage.getItem("_sniff_key") }
     });
     if(f.status !== 200) return alert("Couldn't fetch timeline!");
@@ -31,6 +31,16 @@ const pullTimeline = async () => {
 if(!localStorage.getItem("_sniff_key"))
     alert("Key not set! Authenticate at /auth first");
 else (async () => {
-    await pullTimeline();
     document.querySelector("#more").addEventListener("click", () => pullTimeline());
+    await pullTimeline();
+
+    document.querySelector("#from").valueAsNumber = Date.now();
+    document.querySelector("#to").valueAsNumber = Date.now();
+
+    document.querySelector("#refresh").addEventListener("click", () => {
+        const params = new URLSearchParams(location.search);
+        params.set("from", document.querySelector("#from").valueAsNumber);
+        params.set("to", document.querySelector("#to").valueAsNumber);
+        location.search = "?" + params.toString();
+    });
 })();
